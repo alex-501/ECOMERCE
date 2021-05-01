5120e7e8c943b780cba823eae7f37545f05aaacd
alex-501 <68447140+alex-501@users.noreply.github.com>
2021-04-30 18:28:01 -0700

    more

---
+++ routes/api/post-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+// get all users
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+
+        attributes: ['id', 'post_url', 'title', 'created_at'],
+      // Query configuration
+    })
+  
+  });
